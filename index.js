b6e10dc37c00d563ae065f770a683ae91195cc23
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-10-02 16:47:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(e => {
+	return e.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(e => {
+	return e[0] === string[0];
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(e => {
+	return e.name === string;
+    })
+}
+
